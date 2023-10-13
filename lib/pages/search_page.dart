75b85670527fc56286f155e34247188b0a63faee
Steeven Michael Armijos Bravo <ms96armijos@gmail.com>
2023-10-13 11:50:25 -0500

    Search Page
    Implement Dropdown Buttons for select cities

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:bus_reservation_udemy/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -8,8 +9,77 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String? fromCity, toCity;
+
+  DateTime? departureDate;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: Form(
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8),
+            children: [
+              DropdownButtonFormField<String>(
+                value: fromCity,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return emptyFieldErrMessage;
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white)),
+                hint: const Text('From'),
+                isExpanded: true,
+                items: cities
+                    .map((city) => DropdownMenuItem<String>(
+                          value: city,
+                          child: Text(city),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    fromCity = value;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              DropdownButtonFormField<String>(
+                value: toCity,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return emptyFieldErrMessage;
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white)),
+                hint: const Text('To'),
+                isExpanded: true,
+                items: cities
+                    .map((city) => DropdownMenuItem<String>(
+                          value: city,
+                          child: Text(city),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    toCity = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
